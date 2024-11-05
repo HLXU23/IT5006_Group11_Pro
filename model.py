36e76b435c09099c8833fdfb978fabc03ac3de1d
HLXU23 <hailingxu23@gmail.com>
2024-11-05 11:16:03 +0800

    Initial Update

---
+++ model.py
@@ -0,0 +1,111 @@
+import torch
+from torch import nn
+
+ori_feature_num = 17
+handcrafted_feature_num = 34
+
+class Model_CNN(nn.Module):
+    def __init__(self, window_size):
+        super(Model_CNN, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(4, ori_feature_num), stride=1),
+            nn.BatchNorm2d(num_features=8),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=(2, 1), stride=2),
+            nn.Conv2d(in_channels=8, out_channels=14, kernel_size=(3, 1), stride=1),
+            nn.BatchNorm2d(num_features=14),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=(2, 1), stride=2),
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=14 * (window_size - 10) // 4, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=1)
+        )
+
+    # Defining the forward pass
+    def forward(self, inputs, handcrafted_feature):
+        x = inputs.unsqueeze(1)
+        x = self.cnn(x)
+        x = x.view(x.size(0), -1)
+        out = self.linear(x)
+        return out
+
+class Model_CNN_feature(nn.Module):
+    def __init__(self):
+        super(Model_CNN, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(4, ori_feature_num), stride=1),
+            nn.BatchNorm2d(num_features=8),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=(2, 1), stride=2),
+            nn.Conv2d(in_channels=8, out_channels=14, kernel_size=(3, 1), stride=1),
+            nn.BatchNorm2d(num_features=14),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=(2, 1), stride=2),
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=70, out_features=1)
+        )
+
+    # Defining the forward pass
+    def forward(self, inputs, handcrafted_feature):
+        x = inputs.unsqueeze(1)
+        x = self.cnn(x)
+        x = x.view(x.size(0), -1)
+        out = self.linear(x)
+        return out
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.lstm = nn.LSTM(batch_first=True, input_size=ori_feature_num, hidden_size=50, num_layers=1)
+        self.attenion = Attention3dBlock()
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=1500, out_features=50),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=50, out_features=10),
+            nn.ReLU(inplace=True)
+        )
+        self.handcrafted = nn.Sequential(
+            nn.Linear(in_features=handcrafted_feature_num, out_features=10),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.2)
+        )
+
+        self.output = nn.Sequential(
+            nn.Linear(in_features=20, out_features=1)
+        )
+
+    def forward(self, inputs, handcrafted_feature):
+        y = self.handcrafted(handcrafted_feature)
+        x, (hn, cn) = self.lstm(inputs)
+        x = self.attenion(x)
+        # flatten
+        x = x.reshape(-1, 1500)
+        x = self.linear(x)
+        out = torch.concat((x, y), dim=1)
+        out = self.output(out)
+        return out
+
+
+class Attention3dBlock(nn.Module):
+    def __init__(self):
+        super(Attention3dBlock, self).__init__()
+
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=30, out_features=30),
+            nn.Softmax(dim=2),
+        )
+
+    # inputs: batch size * window size(time step) * lstm output dims
+    def forward(self, inputs):
+        x = inputs.permute(0, 2, 1)
+        x = self.linear(x)
+        x_probs = x.permute(0, 2, 1)
+        # print(torch.sum(x_probs.item()))
+        output = x_probs * inputs
+        return output
---
+++ train.py
@@ -0,0 +1,104 @@
+import torch
+import numpy as np
+
+class Trainer:
+    def __init__(self, model, model_optimizer, print_every, epochs=200, device='cpu', prefix='FD001', result_name='RUL_pred', max_rul = 125):
+        self.model = model.to(device)
+        self.model_optimizer = model_optimizer
+        self.print_every = print_every
+        self.epochs = epochs
+        self.device = device
+        self.criterion = torch.nn.MSELoss()
+        self.prefix = prefix
+        self.log_file = f'./log/{result_name}.log'
+        self.max_rul = max_rul
+
+    def write_train_log(self, log_message):
+        print(log_message)
+        with open(self.log_file, "a") as f:
+            f.write(log_message + "\n")
+
+    def train_single_epoch(self, dataloader):
+        running_loss = 0
+
+        length = len(dataloader)
+
+        for batch_index, data in enumerate(dataloader, 0):
+            inputs, handcrafted_feature, labels = data
+            inputs, handcrafted_feature, labels = inputs.to(self.device), handcrafted_feature.to(self.device), labels.to(self.device)
+            self.model_optimizer.zero_grad()
+            predictions = self.model(inputs, handcrafted_feature)
+            loss = self.criterion(predictions, labels)
+            running_loss += loss.item()
+            loss.backward()
+
+            self.model_optimizer.step()
+
+            if (batch_index + 1) % self.print_every == 0:
+                log_message = 'batch:{}/{}, loss(avg. on {} batches: {}'.format(batch_index + 1,
+                                                                        length,
+                                                                        self.print_every,
+                                                                        running_loss / self.print_every,
+                                                                        )
+                self.write_train_log(log_message)
+                running_loss = 0
+
+    def train(self, train_loader, test_loader, iteration):
+        for epoch in range(self.epochs):
+            log_message = 'Epoch: {}'.format(epoch + 1)
+            self.write_train_log(log_message)
+            self.model.train()
+            self.train_single_epoch(train_loader)
+            current_score, current_RMSE = self.test(test_loader)
+            if epoch == 0:
+                best_score = current_score
+                best_RMSE = current_RMSE
+            else:
+                if current_score < best_score:
+                    best_score = current_score
+                    self.save_checkpoints(iteration + 1, epoch + 1, 'best_score')
+                if current_RMSE < best_RMSE:
+                    best_RMSE = current_RMSE
+                    self.save_checkpoints(iteration + 1, epoch + 1, 'best_RMSE')
+        return float(best_score), float(best_RMSE)
+
+    def save_checkpoints(self, iteration, epoch, which_type):
+        state = {
+            'iter': iteration,
+            'epoch': epoch,
+            'state_dict': self.model.state_dict(),
+            'optim_dict': self.model_optimizer.state_dict()
+        }
+        torch.save(state, './checkpoints/{}_iteration{}_{}.pth.tar'.format(self.prefix, iteration, which_type))
+        log_message = '{}_checkpoints saved successfully!'.format(which_type)
+        self.write_train_log(log_message)
+
+    @staticmethod
+    def score(y_true, y_pred):
+        score = 0
+        y_true = y_true.cpu()
+        y_pred = y_pred.cpu()
+        for i in range(len(y_pred)):
+            if y_true[i] <= y_pred[i]:
+                score = score + np.exp(-(y_true[i] - y_pred[i]) / 10.0) - 1
+            else:
+                score = score + np.exp((y_true[i] - y_pred[i]) / 13.0) - 1
+        return score
+
+    def test(self, test_loader):
+        score = 0
+        loss = 0
+        self.model.eval()
+        criterion = torch.nn.MSELoss()
+        for batch_index, data in enumerate(test_loader, 0):
+            with torch.no_grad():
+                inputs, handcrafted_feature, labels = data
+                inputs, handcrafted_feature, labels = inputs.to(self.device), handcrafted_feature.to(self.device), labels.to(self.device)
+                predictions = self.model(inputs, handcrafted_feature)
+
+                score += self.score(labels * self.max_rul, predictions * self.max_rul)
+                loss += criterion(labels * self.max_rul, predictions * self.max_rul) * len(labels)
+        loss = (loss / len(test_loader.dataset)) ** 0.5
+        log_message = 'test result: score: {}, RMSE: {}'.format(score.item(), loss)
+        self.write_train_log(log_message)
+        return score.item(), loss
